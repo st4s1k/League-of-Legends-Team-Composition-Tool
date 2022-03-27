6a585211a1a8abd28f9f1530352aef3027a7982d
Stanislav <st4s1k24@gmail.com>
2022-03-27 21:32:25 +0300

    Visual overhaul

--- src/main/java/com/st4s1k/leagueteamcomp/repository/ChampionRepository.java
+++ src/main/java/com/st4s1k/leagueteamcomp/repository/ChampionRepository.java
@@ -5,39 +5,52 @@ import com.st4s1k.leagueteamcomp.model.champion.Champions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 public class ChampionRepository {
 
     private static ChampionRepository INSTANCE;
-    private final Champions champions;
+    private final CompletableFuture<Champions> champions;
 
     public static ChampionRepository getInstance() {
         return INSTANCE;
     }
 
-    private ChampionRepository(Champions champions) {
+    private ChampionRepository(CompletableFuture<Champions> champions) {
         this.champions = champions;
     }
 
-    public static void init(Champions champions) {
+    public static void init(CompletableFuture<Champions> champions) {
         if (INSTANCE == null) {
             INSTANCE = new ChampionRepository(champions);
         }
     }
 
     public List<String> getAllChampionKeys() {
-        return new ArrayList<>(champions.getChampions().keySet());
+        return champions
+            .thenApply(Champions::getChampions)
+            .thenApply(Map::keySet)
+            .thenApply(ArrayList::new)
+            .getNow(new ArrayList<>());
     }
 
     public Optional<Champion> findChampionDataByKey(String championKey) {
-        return Optional.ofNullable(champions.getChampions().get(championKey));
+        return champions
+            .thenApply(Champions::getChampions)
+            .thenApply(map -> Optional.ofNullable(map.get(championKey)))
+            .getNow(Optional.empty());
     }
 
     public Optional<Champion> findChampionDataByName(String championName) {
-        return champions.getChampions().values().stream()
-            .filter(championData -> championData.getName().equals(championName))
-            .findFirst();
+        return champions
+            .thenApply(Champions::getChampions)
+            .thenApply(Map::values)
+            .thenApply(values -> values.stream()
+                .filter(championData -> championData.getName().equals(championName))
+                .findFirst())
+            .getNow(Optional.empty());
     }
 
     public boolean existsChampionDataByName(String championName) {
