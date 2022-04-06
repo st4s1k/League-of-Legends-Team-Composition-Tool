e25d6490777c5b860ea9ef5d46f5b921ee969c6b
Stanislav <st4s1k24@gmail.com>
2022-04-06 22:56:58 +0300

    Sync with develop

--- src/main/java/com/st4s1k/leagueteamcomp/LeagueTeamCompStageInitializer.java
+++ src/main/java/com/st4s1k/leagueteamcomp/LeagueTeamCompStageInitializer.java
@@ -8,9 +8,6 @@ import com.st4s1k.leagueteamcomp.controller.LeagueTeamCompController;
 import com.st4s1k.leagueteamcomp.exceptions.LTCException;
 import com.st4s1k.leagueteamcomp.model.champion.ChampionDTO;
 import com.st4s1k.leagueteamcomp.repository.ChampionRepository;
-import com.st4s1k.leagueteamcomp.utils.ResizeHelper;
-import com.st4s1k.leagueteamcomp.utils.Resources;
-import com.st4s1k.leagueteamcomp.utils.Utils;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -29,8 +26,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -41,6 +36,9 @@ import java.util.stream.DoubleStream;
 
 import static com.merakianalytics.orianna.types.common.Region.EUROPE_WEST;
 import static com.st4s1k.leagueteamcomp.LeagueTeamCompStarter.StageReadyEvent;
+import static com.st4s1k.leagueteamcomp.utils.ResizeHelper.addResizeListener;
+import static com.st4s1k.leagueteamcomp.utils.Resources.*;
+import static com.st4s1k.leagueteamcomp.utils.Utils.setFieldValue;
 import static java.net.http.HttpResponse.BodyHandlers;
 import static java.util.Objects.requireNonNull;
 
@@ -58,34 +56,39 @@ public class LeagueTeamCompStageInitializer implements ApplicationListener<Stage
     @SneakyThrows
     public void onApplicationEvent(StageReadyEvent event) {
         Stage stage = event.getStage();
-        Orianna.setRiotAPIKey(Resources.RIOT_API_KEY);
+        Orianna.loadConfiguration("orianna-config.json");
         Orianna.setDefaultRegion(EUROPE_WEST);
         Thread.setDefaultUncaughtExceptionHandler(this::showError);
         getChampionsFromUrl();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(Resources.FXML_FILE_PATH), Resources.LTC_VIEW_PROPERTIES);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_FILE_PATH), LTC_VIEW_PROPERTIES);
         loader.setControllerFactory(applicationContext::getBean);
         Parent root = loader.load();
         LeagueTeamCompController controller = loader.getController();
         controller.setStageAndSetupListeners(stage);
-        controller.setCloseButtonAction(() -> Utils.stop(LeagueTeamCompStageInitializer.class).accept(() -> {
-            controller.stop();
-            stage.close();
-        }));
+        controller.setCloseButtonAction(() -> applicationStopAction(stage, controller));
         controller.setMinimizeButtonAction(() -> stage.setIconified(true));
-        Scene scene = new Scene(root, Resources.WINDOW_WIDTH, Resources.WINDOW_HEIGHT);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.initStyle(StageStyle.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
-        stage.getIcons().add(new Image(requireNonNull(getClass().getResourceAsStream(Resources.ICON_FILE_PATH))));
-        stage.setTitle(Resources.WINDOW_TITLE);
-        stage.setResizable(Resources.WINDOW_IS_RESIZABLE);
+        stage.getIcons().add(new Image(requireNonNull(getClass().getResourceAsStream(ICON_FILE_PATH))));
+        stage.setTitle(WINDOW_TITLE);
+        stage.setResizable(WINDOW_IS_RESIZABLE);
         stage.show();
+    }
+
+    private void applicationStopAction(Stage stage, LeagueTeamCompController controller) {
+        String className = getClass().getSimpleName();
+        log.info("Stopping {}...", className);
+        controller.stop();
+        stage.close();
+        log.info("{} stopped.", className);
     }
 
     @SneakyThrows
     private void getChampionsFromUrl() {
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(new URI(Resources.CHAMPIONS_URL))
+            .uri(new URI(CHAMPIONS_URL))
             .build();
         HttpClient.newHttpClient()
             .sendAsync(request, BodyHandlers.ofString())
@@ -96,7 +99,7 @@ public class LeagueTeamCompStageInitializer implements ApplicationListener<Stage
     private Map<String, ChampionDTO> getChampions(HttpResponse<String> response) {
         Map<String, ChampionDTO> champions = gson.fromJson(response.body(), getChampionMapType());
         champions.values().forEach(champion ->
-            Utils.setFieldValue(champion, "image", new Image(champion.getIconUrl(), true)));
+            setFieldValue(champion, "image", new Image(champion.getIconUrl(), true)));
         return champions;
     }
 
@@ -106,34 +109,24 @@ public class LeagueTeamCompStageInitializer implements ApplicationListener<Stage
     }
 
     private void showError(Thread t, Throwable e) {
-        log.error(e.getMessage(), e);
-        if (Platform.isFxApplicationThread()) {
-            showErrorDialog(e);
+        if (Platform.isFxApplicationThread() && e instanceof LTCException ltcException) {
+            showErrorDialog(ltcException);
         } else {
-            log.error("An unexpected error occurred in " + t);
+            log.error(e.getMessage(), e);
         }
     }
 
     @SneakyThrows
-    private void showErrorDialog(Throwable e) {
-        StringWriter errorMsg = new StringWriter();
-        e.printStackTrace(new PrintWriter(errorMsg));
+    private void showErrorDialog(LTCException ltcException) {
         Stage dialog = new Stage();
         dialog.initModality(Modality.APPLICATION_MODAL);
-        FXMLLoader loader = new FXMLLoader(LeagueTeamCompStageInitializer.class.getResource("ltc-exception.fxml"));
-        loader.setControllerFactory(applicationContext::getBean);
+        FXMLLoader loader = new FXMLLoader(LeagueTeamCompApplication.class.getResource("ltc-exception.fxml"));
         Region root = loader.load();
         LTCExceptionController controller = loader.getController();
         controller.setStageAndSetupListeners(dialog);
-        int sceneWidth = 600;
-        int sceneHeight = 400;
-        if (e instanceof LTCException ltcException) {
-            sceneWidth = 300;
-            sceneHeight = 150;
-            controller.setErrorText(ltcException.getMessage());
-        } else {
-            controller.setErrorText(errorMsg.toString());
-        }
+        int sceneWidth = 400;
+        int sceneHeight = 150;
+        controller.setErrorText(ltcException.getMessage());
         Scene scene = new Scene(root, sceneWidth, sceneHeight);
         scene.setFill(Color.TRANSPARENT);
         dialog.initStyle(StageStyle.TRANSPARENT);
@@ -143,7 +136,7 @@ public class LeagueTeamCompStageInitializer implements ApplicationListener<Stage
         Insets padding = root.getPadding();
         double border = DoubleStream.of(padding.getTop(), padding.getRight(), padding.getBottom(), padding.getLeft())
             .min().orElse(4);
-        ResizeHelper.addResizeListener(dialog, border);
+        addResizeListener(dialog, border);
         dialog.show();
     }
 }
